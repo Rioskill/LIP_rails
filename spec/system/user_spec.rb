04e4e2eafbd807afd1a6457b92c6008e1dc1fe07
nikstarling <rioskill@yandex.ru>
2022-12-20 10:43:12 +0300

    lab 12 system test

---
+++ spec/system/user_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe "application", type: :system do
+  it 'should redirect to to new_user_path' do
+    visit root_path
+
+    click_link 'Sign Up'
+    expect(page).to have_current_path new_user_path
+  end
+
+  it 'should create new user and login' do
+    visit root_path
+
+    click_link 'Sign Up'
+
+    fill_in 'Name', with: 'test_user_name'
+    fill_in 'Password', with: 'test_user_password'
+    fill_in 'Password confirmation', with: 'test_user_password'
+
+    click_button 'Create User'
+
+    expect(page).to have_current_path root_path
+
+    fill_in 'Name', with: 'test_user_name'
+    fill_in 'Password', with: 'test_user_password'
+
+    click_button 'Login'
+    expect(page).to have_current_path input_path
+
+    fill_in "start", with: 1
+    fill_in "end", with:10
+
+    click_button "Посчитать"
+
+    expect(page).to have_current_path input_path
+    expect(page).to have_css 'table'
+    expect(all("td").map { |val| val.text }).to eq([1, 2, 3, 3, 6,
+                                                    2, 3, 4, 4, 24,
+                                                    4, 5, 6, 5, 120,
+                                                    8, 9, 10, 6, 720].map(&:to_s))
+  end
+end
